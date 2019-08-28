448573039413374e104fd3720e9262c8f31d4f90
LiudmilaZyrianova <38331313+LiudmilaZyrianova@users.noreply.github.com>
2019-08-28 14:00:42 -0400

    Two Sum - E

---
+++ Problem1.java
@@ -0,0 +1,19 @@
+//1. Two Sum - E. Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[] { map.get(complement), i };
+        }
+        map.put(nums[i], i);
+    }
+    throw new IllegalArgumentException("No two sum solution");
+    }
+}
