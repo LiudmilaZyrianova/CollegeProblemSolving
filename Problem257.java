d6c08662918507798155e31a2dc336283bb15369
LiudmilaZyrianova <38331313+LiudmilaZyrianova@users.noreply.github.com>
2019-09-18 14:56:22 -0400

    Binary Tree Paths - E

--- Problem257.java
+++ Problem257.java
@@ -13,7 +13,17 @@ public class Main {
       TreeNode(int x) { val = x; }
     }
 
-
+    //Better solution
+//public List<String> binaryTreePaths(TreeNode root) {
+//     List<String> answer = new ArrayList<String>();
+//     if (root != null) searchBT(root, "", answer);
+//     return answer;
+// }
+// private void searchBT(TreeNode root, String path, List<String> answer) {
+//     if (root.left == null && root.right == null) answer.add(path + root.val);
+//     if (root.left != null) searchBT(root.left, path + root.val + "->", answer);
+//     if (root.right != null) searchBT(root.right, path + root.val + "->", answer);
+// }
     public static void traverse(TreeNode node, List<String> ls, StringBuilder sb){
         sb.append(node.val);
         sb.append("->");
