e91a96f47297142c8b6b9ce5cc0ef0807107a9bd
LiudmilaZyrianova <38331313+LiudmilaZyrianova@users.noreply.github.com>
2022-04-09 23:57:37 -0400

    79. Word Search - M

---
+++ Problem79.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean existsWithVisited(char[][] board, boolean[][] visited, String word, int i, int j, int index) {
+        if (i >= board.length || i < 0 || j >= board[0].length || j < 0) {return false;}
+        
+        if (word.length() == index) {return true;}
+        
+        if (!visited[i][j] && board[i][j] == word.charAt(index)) {
+            visited[i][j] = true;
+            if (word.length() == index + 1) {return true;}
+            if (existsWithVisited(board, visited, word, i+1, j, index + 1)) {return true;}
+            if (existsWithVisited(board, visited, word, i-1, j, index + 1)) {return true;}
+            if (existsWithVisited(board, visited, word, i, j+1, index + 1)) {return true;}
+            if (existsWithVisited(board, visited, word, i, j-1, index + 1)) {return true;}
+            visited[i][j] = false;
+        }
+        return false;
+    }
+    
+    public boolean exist(char[][] board, String word) {
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (existsWithVisited(board, visited, word, i, j, 0)) return true;
+            }
+        }
+        
+        return false;
+    }
+}
