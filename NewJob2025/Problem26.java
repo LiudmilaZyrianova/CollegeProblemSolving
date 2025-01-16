2c77da54d7c8c482fda1e8d960c73321ac166b88
LiudmilaZyrianova <38331313+LiudmilaZyrianova@users.noreply.github.com>
2025-01-16 15:38:56 -0600

    26. Remove Duplicates from Sorted Array
    with better solution

--- NewJob2025/Problem26.java
+++ NewJob2025/Problem26.java
@@ -18,13 +18,11 @@ public int removeDuplicates(int[] nums) {
 
 
 // better solution without storing currentNum:
-    int removeDuplicates(vector<int>& nums) {
-        int j = 1;
-        for(int i = 1; i < nums.size(); i++){
-            if(nums[i] != nums[i - 1]){
-                nums[j] = nums[i];
-                j++;
-            }
-        }
-        return j;
-    }
+// Better solution
+public int removeDuplicates(int[] nums) {
+    int i = nums.length > 0 ? 1 : 0;
+    for (int n : nums)
+        if (n > nums[i-1])
+            nums[i++] = n;
+    return i;
+}
