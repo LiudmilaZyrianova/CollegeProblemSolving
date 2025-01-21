b56b2cc93bc2841a0f58a4ec059acc66a9939b6f
LiudmilaZyrianova <38331313+LiudmilaZyrianova@users.noreply.github.com>
2025-01-21 12:04:59 -0600

    121. Best Time to Buy and Sell Stock

---
+++ NewJob2025/Problem121.java
@@ -0,0 +1,17 @@
+// 121. Best Time to Buy and Sell Stock
+// update buy var if lower
+// updates profit var if profit becomes better using current as a seller
+        int buy = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else if (prices[i] - buy > profit) {
+                profit = prices[i] - buy;
+            }
+        }
+        return profit;
+
+///
+Kadane's Algorithm
+Kadane's Algorithm is a dynamic programming technique used to find the maximum subarray sum in an array of numbers. The algorithm maintains two variables: max_current represents the maximum sum ending at the current position, and max_global represents the maximum subarray sum encountered so far. At each iteration, it updates max_current to include the current element or start a new subarray if the current element is larger than the accumulated sum. The max_global is updated if max_current surpasses its value.
