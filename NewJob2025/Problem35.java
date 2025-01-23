cdec2fee7e4fde572436f97fb5e46452df01ad18
LiudmilaZyrianova <38331313+LiudmilaZyrianova@users.noreply.github.com>
2025-01-23 15:32:11 -0600

    35. Search Insert Position

---
+++ NewJob2025/Problem35.java
@@ -0,0 +1,17 @@
+// 35. Search Insert Position
+
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low  + ((high - low) / 2);
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else if (nums[mid] == target) {
+                return mid;
+            } 
+        }
+        return (high == -1) ? 0 : low;
+    }
