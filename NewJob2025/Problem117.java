4abadd11bc353771111f3f8efbf40294fcc098a1
LiudmilaZyrianova <38331313+LiudmilaZyrianova@users.noreply.github.com>
2025-02-06 10:53:05 -0600

    117. Populating Next Right Pointers in Each Node II - M

---
+++ NewJob2025/Problem117.java
@@ -0,0 +1,57 @@
+// 117. Populating Next Right Pointers in Each Node II - M
+    public Node connect(Node root) {
+        //bfs
+        if (root == null) return root;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            Node prev = null;
+            for (int i=0; i < size; i++) {
+                Node curr = queue.poll();
+                curr.next = prev;
+                
+                // start right to left
+                if (curr.right != null) queue.add(curr.right);
+                if (curr.left != null) queue.add(curr.left);
+                prev = curr;
+            }
+        }
+        return root;
+    }
+
+// better solution w no extra mem
+        TreeLinkNode head = null; //head of the next level
+        TreeLinkNode prev = null; //the leading node on the next level
+        TreeLinkNode cur = root;  //current node of current level
+
+        while (cur != null) {
+            
+            while (cur != null) { //iterate on the current level
+                //left child
+                if (cur.left != null) {
+                    if (prev != null) {
+                        prev.next = cur.left;
+                    } else {
+                        head = cur.left;
+                    }
+                    prev = cur.left;
+                }
+                //right child
+                if (cur.right != null) {
+                    if (prev != null) {
+                        prev.next = cur.right;
+                    } else {
+                        head = cur.right;
+                    }
+                    prev = cur.right;
+                }
+                //move to next node
+                cur = cur.next;
+            }
+            
+            //move to next level
+            cur = head;
+            head = null;
+            prev = null;
+        }
